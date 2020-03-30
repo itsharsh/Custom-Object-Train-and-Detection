9b08a0990b9f7c0e3d8aec9290c4bc2f48f04569
Harsh <hkumarsingh142@gmail.com>
2020-03-30 19:01:48 +0530

    Try catch added

--- PTZ_Control.py
+++ PTZ_Control.py
@@ -1,38 +1,46 @@
 import serial
 # serialPort = "/dev/ttyUSB0"  # For Linux
 serialPort = "COM8"  # For Windows
-baudRate = 9600
+baudRate = 115200
 
-ser = serial.Serial(
-    port=serialPort,
-    baudrate=baudRate,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.EIGHTBITS
-)
 
-print(ser.is_open)
+def sendDataToSerial(data):
+    try:
+        ser = serial.Serial(
+            port=serialPort,
+            baudrate=baudRate,
+            parity=serial.PARITY_ODD,
+            stopbits=serial.STOPBITS_TWO,
+            bytesize=serial.EIGHTBITS
+        )
+        print(ser.is_open)
+        print(ser.name)         # check which port was really used
 
-print(ser.name)         # check which port was really used
+        ser.write(data)
+        ser.close()
+
+    except serial.serialutil.SerialException as e:
+        print("Could not open COM Port")
+
 
 #0-Manual, 1-Automatic
 MR_Bit = '1'
 
 #0-Disable, 1-Enable
-LE_Bit = '1'
-TCE_Bit = '0'
+LE_Bit = '0'
+TCE_Bit = '0'  # Thermal Camera
 ZCE_Bit = '0'
 
-PE_Bit = '0'
+PE_Bit = '1'
 TE_Bit = '0'
 ZE_Bit = '0'
 FE_Bit = '0'
 
 #0-Right/Down/Out, 1-Left/Up/In
-PLR_Bit = '0'
-TUD_Bit = '0'
-ZIO_Bit = '0'
-FIO_Bit = '0'
+PLR_Bit = '1'   # Depends on PE_Bit
+TUD_Bit = '1'   # Depends on TE_Bit
+ZIO_Bit = '1'   # Depends on ZCE_Bit
+FIO_Bit = '1'   # Depends on ZCE_Bit
 
 data = FIO_Bit+ZIO_Bit+TUD_Bit+PLR_Bit+FE_Bit + \
     ZE_Bit+TE_Bit+PE_Bit+ZCE_Bit+TCE_Bit+LE_Bit+MR_Bit
@@ -40,6 +48,4 @@ data = FIO_Bit+ZIO_Bit+TUD_Bit+PLR_Bit+FE_Bit + \
 print(data)
 data = int(data, 2)
 print(data)
-
-ser.write(data)
-ser.close()
+sendDataToSerial(data)
