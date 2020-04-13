0788ca0e13f6051eb419906792500aa0f87a476c
Hulk Buster <vivek06sharma11@gmail.com>
2020-04-13 12:05:34 +0530

    add path_config file

--- get_camera_feed.py
+++ get_camera_feed.py
@@ -1,23 +1,11 @@
 import cv2
 import time
 from datetime import datetime
+import path_config
 
+videoPath=path_config.testVideos
 Streams=int(input('Stream Classes:'))
-
-camera0Source = "/home/vivek/AdTracker/test1.mp4"
-camera1Source = "/home/vivek/AdTracker/test2.mp4"
-camera2Source = "/home/vivek/AdTracker/test3.mp4"
-camera3Source = "/home/vivek/AdTracker/test4.mp4"
-camera4Source = "/home/vivek/AdTracker/test5.mp4"
-camera5Source = "/home/vivek/AdTracker/test6.mp4"
-
-camera6Source = "/home/vivek/AdTracker/test7.mp4"
-camera7Source = "/home/vivek/AdTracker/test8.mp4"
-camera8Source = "/home/vivek/AdTracker/test9.mp4"
-camera9Source = "/home/vivek/AdTracker/test10.mp4"
-camera10Source = "/home/vivek/AdTracker/test11.mp4"
-camera11Source = "/home/vivek/AdTracker/test12.mp4"
-
+cameraSource=path_config.cameraSource
 
 for i in range(Streams):
-    exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(camera"+str(i)+"Source)\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".jpeg\", frame)\n\t\t\tyield jpeg.tobytes()\n\t\t\tif(type(camera"+str(i)+"Source) is str):\n\t\t\t\ttime.sleep(0.040)")
+    exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(videoPath+cameraSource["+str(i)+"])\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".jpeg\", frame)\n\t\t\tyield jpeg.tobytes()\n\t\t\tif(type(cameraSource["+str(i)+"]) is str):\n\t\t\t\ttime.sleep(0.040)")
---
+++ path_config.py
@@ -0,0 +1,11 @@
+import os
+import platform
+
+if platform.system() == "Windows":
+    adTrackerDir = "D:/Test/"
+    gitRepoDir = os.path.join(
+        "D:/Office/Google Drive/Projects/AI/AdTracker", "O2i-Adtracker")
+elif platform.system() == "Linux":
+    testVideos = "/home/vivek/Test_Videos/"
+    gitRepoDir = os.path.join("~/", "Test_Videos")
+    cameraSource = ["test1.mp4","test2.mp4","test3.mp4","test4.mp4","test5.mp4","test6.mp4","test7.mp4","test8.mp4","test9.mp4","test10.mp4","test11.mp4","test12.mp4"]
