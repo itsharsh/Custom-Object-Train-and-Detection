18e012f00075b014a4c0de05a93d9fddf40b51d5
Hulk Buster <vivek06sharma11@gmail.com>
2020-04-13 14:53:26 +0530

    live and saved video stream

--- get_camera_feed.py
+++ get_camera_feed.py
@@ -7,5 +7,10 @@ videoPath=path_config.testVideos
 Streams=int(input('Stream Classes:'))
 cameraSource=path_config.cameraSource
 
+
+
 for i in range(Streams):
-    exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(videoPath+cameraSource["+str(i)+"])\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".jpeg\", frame)\n\t\t\tyield jpeg.tobytes()\n\t\t\tif(type(cameraSource["+str(i)+"]) is str):\n\t\t\t\ttime.sleep(0.040)")
+    if type(cameraSource[i])==int:
+        exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(cameraSource["+str(i)+"])\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".jpeg\", frame)\n\t\t\tyield jpeg.tobytes()\n\t\t\tif(type(cameraSource["+str(i)+"]) is str):\n\t\t\t\ttime.sleep(0.040)")
+    else:
+        exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(videoPath+cameraSource["+str(i)+"])\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".jpeg\", frame)\n\t\t\tyield jpeg.tobytes()\n\t\t\tif(type(cameraSource["+str(i)+"]) is str):\n\t\t\t\ttime.sleep(0.040)")
