dc9e87cf720e995c848edbc00513f18b84b5c865
Hulk Buster <vivek06sharma11@gmail.com>
2020-04-20 17:34:20 +0530

    must change path in path_config.py

--- get_camera_feed.py
+++ get_camera_feed.py
@@ -11,6 +11,6 @@ cameraSource=path_config.cameraSource
 
 for i in range(Streams):
     if type(cameraSource[i])==int:
-        exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(cameraSource["+str(i)+"])\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".jpeg\", frame)\n\t\t\tyield jpeg.tobytes()\n\t\t\tif(type(cameraSource["+str(i)+"]) is str):\n\t\t\t\ttime.sleep(0.040)")
+        exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(cameraSource["+str(i)+"])\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".png\", frame)\n\t\t\tyield jpeg.tostring()\n\t\t\tif(type(cameraSource["+str(i)+"]) is str):\n\t\t\t\ttime.sleep(0.040)")
     else:
-        exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(videoPath+cameraSource["+str(i)+"])\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".jpeg\", frame)\n\t\t\tyield jpeg.tobytes()\n\t\t\tif(type(cameraSource["+str(i)+"]) is str):\n\t\t\t\ttime.sleep(0.040)")
+        exec("class Camera"+str(i)+"():\n\tcamera=cv2.VideoCapture(videoPath+cameraSource["+str(i)+"])\n\t@staticmethod\n\tdef frames():\n\t\twhile True:\n\t\t\t_, frame = Camera"+str(i)+".camera.read()\n\t\t\t_, jpeg = cv2.imencode(\".png\", frame)\n\t\t\tyield jpeg.tostring()\n\t\t\tif(type(cameraSource["+str(i)+"]) is str):\n\t\t\t\ttime.sleep(0.040)")
--- path_config.py
+++ path_config.py
@@ -8,4 +8,5 @@ if platform.system() == "Windows":
 elif platform.system() == "Linux":
     testVideos = "/home/vivek/Test_Videos/"
     gitRepoDir = os.path.join("~/", "Test_Videos")
-    cameraSource = [0,"test2.mp4","test3.mp4","test4.mp4","test5.mp4","test6.mp4","test7.mp4","test8.mp4","test9.mp4","test10.mp4","test11.mp4","test12.mp4"]
+
+cameraSource = ["test1.mp4","test2.mp4","test3.mp4","test4.mp4","test5.mp4","test6.mp4","test7.mp4","test8.mp4","test9.mp4","test10.mp4","test11.mp4","test12.mp4"]
